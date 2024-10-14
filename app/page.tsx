7f155a9c23f2df6580a7762784f7fc9929091c4b
gokm8 <gouzb22@student.sdu.dk>
2024-10-15 01:23:54 +0200

    add another hero (heroTwo)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AboutUs from '@/components/home-page/about-us'
 import Hero from '@/components/home-page/hero'
+import { HeroTwo } from '@/components/home-page/heroTwo'
 import LogoCarousel from '@/components/home-page/logo-carousel'
 import Testimonials from '@/components/home-page/testimonials'
 import Image from 'next/image'
@@ -7,7 +8,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <Hero />
+      <HeroTwo />
       <AboutUs />
       <LogoCarousel />
       <Testimonials />
