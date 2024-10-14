e14afe44be3a5431f2071f3e64f4ebeefb1813a6
Gøkmen Øzbayir <107099606+gokm8@users.noreply.github.com>
2024-10-14 17:24:24 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/home-page/hero";
+import Testimonials from "@/components/home-page/testimonials";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <Testimonials />
+        </>
+    );
+}
---
+++ components/home-page/testimonials.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Section, Container } from '@/components/craft'
+
+export default function Testimonials() {
+  return (
+    <Section>
+      <Container>
+        <h2>Testimonials</h2>
+        <p>- Testimonial -</p>
+      </Container>
+    </Section>
+  )
+}
