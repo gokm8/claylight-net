23a595bf3681e923e20b3d6643bbdacdc2310932
gokm8 <gouzb22@student.sdu.dk>
2024-10-15 00:40:32 +0200

    implement logo carousel

--- components/home-page/logo-carousel.tsx
+++ components/home-page/logo-carousel.tsx
@@ -1,16 +1,24 @@
 'use client' // Add this at the top
 
 import { useEffect, useRef } from 'react'
-import { Section, Container } from '../craft'
+import Image from 'next/image'
 
 // Definér logodata uden for komponenten
 const logos = [
-  { src: '/icons8-facebook.svg', alt: 'Facebook' },
-  { src: '/icons8-facebook.svg', alt: 'Facebook' },
-  { src: '/icons8-facebook.svg', alt: 'Facebook' },
-  { src: '/icons8-facebook.svg', alt: 'Facebook' },
-  { src: '/icons8-facebook.svg', alt: 'Facebook' },
-  { src: '/icons8-facebook.svg', alt: 'Facebook' }
+  { src: '/rumors-green.svg', alt: 'Rumors Club', name: 'RUMORS CLUB' },
+  {
+    src: '/roskilde-festival.svg',
+    alt: 'Roskilde Festival',
+    name: 'ROSKILDE FESTIVAL'
+  },
+  {
+    src: '/tivoli-friheden.svg',
+    alt: 'Tivoli Friheden',
+    name: 'TIVOLI FRIHEDEN'
+  },
+  { src: '/Culuturebox.svg', alt: 'Culture Box', name: 'CULTURE BOX' },
+  { src: '/bella-center.svg', alt: 'Bella Center', name: 'BELLA CENTER' },
+  { src: '/Carlsberg-logo.svg', alt: 'Carlsberg', name: 'CARLSBERG' }
 ]
 
 const LogoCarousel = () => {
@@ -26,22 +34,33 @@ const LogoCarousel = () => {
   }, [])
 
   return (
-    <Section className='bg-amber-300'>
-      <Container>
+    <div className='bg-gray-300 py-4 md:py-8'>
+      <div className='mx-auto max-w-5xl'>
         <div className='inline-flex w-full flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]'>
           <ul
             ref={logosRef}
             className='animate-infinite-scroll flex items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8'
           >
             {logos.map((logo, index) => (
-              <li key={index}>
-                <img src={logo.src} alt={logo.alt} />
+              <li
+                key={index}
+                className='flex flex-col items-center justify-center'
+              >
+                <Image
+                  src={logo.src}
+                  alt={logo.alt}
+                  width={125}
+                  height={125}
+                  quality={95}
+                  className='h-[75px] w-auto opacity-80 brightness-0 contrast-50 filter'
+                  loading='lazy'
+                />
               </li>
             ))}
           </ul>
         </div>
-      </Container>
-    </Section>
+      </div>
+    </div>
   )
 }
 
